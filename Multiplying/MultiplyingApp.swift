b549a2f581e2fac3f6574a36e892f58c80da07c6
Patricia <patysiq@yahoo.com.br>
2021-01-22 08:45:40 -0300

    Initial Commit

---
+++ Multiplying/MultiplyingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyingApp.swift
+//  Multiplying
+//
+//  Created by PATRICIA S SIQUEIRA on 22/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
